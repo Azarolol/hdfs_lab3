1508a0704782a89c58b782d12892750258e90a31
Azarolol <gimazovazamat@mail.ru>
2021-11-11 22:04:14 +0300

    Scripted auto-commit on change (2021-11-11 22:04:14) by gitwatch.sh

--- src/main/java/hdfs/lab3/azarolol/MaxDelayTimeCounter.java
+++ src/main/java/hdfs/lab3/azarolol/MaxDelayTimeCounter.java
@@ -12,6 +12,7 @@ public class MaxDelayTime {
         JavaSparkContext sc = new JavaSparkContext(conf);
 
         JavaRDD<String> airports = sc.textFile("L_AIRPORT_ID.csv");
-        JavaRDD<String> flights = sc.textFile("")
+        JavaRDD<String> flights = sc.textFile("664600583_T_ONTIME_sample.csv");
+        
     }
 }
