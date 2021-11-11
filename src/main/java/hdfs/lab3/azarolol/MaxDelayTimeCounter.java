fe60ad324ae8eb1153b0fbbf43d35adc8a07353a
Azarolol <gimazovazamat@mail.ru>
2021-11-11 21:41:29 +0300

    Scripted auto-commit on change (2021-11-11 21:41:29) by gitwatch.sh

--- src/main/java/hdfs/lab3/azarolol/MaxDelayTimeCounter.java
+++ src/main/java/hdfs/lab3/azarolol/MaxDelayTimeCounter.java
@@ -1,7 +1,11 @@
 package hdfs.lab3.azarolol;
 
 public class MaxDelayTime {
+    final static String AppName = "MaxDelayTimeCounter"
     public static void mian (String[] args) throws Exception {
+        SparkConf conf = new SparkConf().setAppName(AppName);
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
         
     }
 }
