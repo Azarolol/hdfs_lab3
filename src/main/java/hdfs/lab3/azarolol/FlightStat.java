a05761dd85dba0edeaeeb06cc83b6bf86fef7d24
Azarolol <gimazovazamat@mail.ru>
2021-11-25 20:33:20 +0300

    Scripted auto-commit on change (2021-11-25 20:33:20) by gitwatch.sh

--- src/main/java/hdfs/lab3/azarolol/FlightStat.java
+++ src/main/java/hdfs/lab3/azarolol/FlightStat.java
@@ -1,2 +1,15 @@
-package hdfs.lab3.azarolol;public class FlightStat {
+package hdfs.lab3.azarolol;
+
+public class FlightStat {
+    private String departureAirport;
+    private String destinationAirport;
+    private boolean ifCancelled;
+    private int delayTime;
+
+    public FlightStat(String departureAirport, String destinationAirport, boolean ifCancelled, int delayTime) {
+        this.departureAirport = departureAirport;
+        this.destinationAirport = destinationAirport;
+        this.delayTime = delayTime;
+        this.ifCancelled = ifCancelled;
+    }
 }
