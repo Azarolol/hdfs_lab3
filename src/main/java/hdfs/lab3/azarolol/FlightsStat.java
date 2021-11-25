2ee96a9cd52e7c5f4c42c12f98c8f58c276b5663
Azarolol <gimazovazamat@mail.ru>
2021-11-25 20:30:21 +0300

    Scripted auto-commit on change (2021-11-25 20:30:21) by gitwatch.sh

--- src/main/java/hdfs/lab3/azarolol/FlightsStat.java
+++ src/main/java/hdfs/lab3/azarolol/FlightsStat.java
@@ -3,5 +3,19 @@ package hdfs.lab3.azarolol;
 import scala.Serializable;
 
 public class FlightsStat implements Serializable {
-    
+    private String departureAirport;
+    private String destinationAirport;
+    private int numberOfDelayedFlights;
+    private int numberOfCancelledFlights;
+    private int maxDelay;
+    private int numberOfFlights;
+
+    public FlightsStat(String departureAirport, String destinationAirport, int numberOfDelayedFlights, int numberOfCancelledFlights, int maxDelay, int numberOfFlights) {
+        this.departureAirport = departureAirport;
+        this.destinationAirport = destinationAirport;
+        this.numberOfDelayedFlights = numberOfDelayedFlights;
+        this.numberOfCancelledFlights = numberOfCancelledFlights;
+        this.maxDelay = maxDelay;
+        this.numberOfFlights = numberOfFlights;
+    }
 }
